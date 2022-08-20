87c16861fdce8b34e4306098c48f80d11fdaa608
KW <kccchiu@yahoo.com>
2022-08-20 18:05:43 -0500

    first

---
+++ README.md
@@ -0,0 +1,12 @@
+# Tensorflow certification exam from coursera
+
+This repo contains the code of my study/review journey with coursera tensorflow developer course. <br>
+Instead of using Google colab like the course, I run python files and notebook locally to replicate the testing environment since the exam require test taker to use PyCharm.
+
+The code contains in this repo is largly the same as the notebooks within the course. The main difference is the path when using the flow_from_directoy function.
+
+## Courses
+- Course 1 - Introduction to TensorFlow for Artificial Intelligence, Machine Learning, and Deep Learning
+- Course 2 - Convolutional Neural Networks in TensorFlow
+- Course 3 - Natural Language Processing in TensorFlow
+- Course 4 - Sequences, Time Series and Prediction
---
+++ course1/week2-lab1.py
@@ -0,0 +1,27 @@
+import tensorflow as tf
+
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+  try:
+    tf.config.experimental.set_virtual_device_configuration(
+        gpus[0],[tf.config.experimental.VirtualDeviceConfiguration(memory_limit=5120)])
+  except RuntimeError as e:
+    print(e)
+
+mnist = tf.keras.datasets.mnist
+
+(training_images, training_labels) ,  (test_images, test_labels) = mnist.load_data()
+
+training_images = training_images/255.0
+test_images = test_images/255.0
+
+model = tf.keras.models.Sequential([tf.keras.layers.Flatten(),
+                                    tf.keras.layers.Dense(1024, activation=tf.nn.relu), # Try experimenting with this layer
+                                    tf.keras.layers.Dense(10, activation=tf.nn.softmax)])
+
+model.compile(optimizer = 'adam',
+              loss = 'sparse_categorical_crossentropy')
+
+model.fit(training_images, training_labels, epochs=5)
+
+print(model.evaluate(test_images, test_labels))
