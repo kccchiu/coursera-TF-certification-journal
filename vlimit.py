386b6fa8171232941fdaa8466d26a1ec35770958
KW <kccchiu@yahoo.com>
2022-08-28 11:22:20 -0500

    change ram limit

--- vlimit.py
+++ vlimit.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
 
-def vram_limit(memory_limit=5120):
+def vram_limit(memory_limit=4190):
     gpus = tf.config.experimental.list_physical_devices('GPU')
     tf.config.experimental.set_virtual_device_configuration(gpus[0],[tf.config.experimental.VirtualDeviceConfiguration(memory_limit=memory_limit)])
